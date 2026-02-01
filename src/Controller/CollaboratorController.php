17451ac13fb13b80221733b2e4b545b7baf46977
mohsennabli <mohsennabli321@gmail.com>
2026-02-01 23:20:25 +0100

    CRUD Created

---
+++ src/Controller/CollaboratorController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Collaborator;
+use App\Form\CollaboratorType;
+use App\Repository\CollaboratorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/collaborator')]
+final class CollaboratorController extends AbstractController
+{
+    #[Route(name: 'app_collaborator_index', methods: ['GET'])]
+    public function index(CollaboratorRepository $collaboratorRepository): Response
+    {
+        return $this->render('collaborator/index.html.twig', [
+            'collaborators' => $collaboratorRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_collaborator_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $collaborator = new Collaborator();
+        $form = $this->createForm(CollaboratorType::class, $collaborator);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($collaborator);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_collaborator_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('collaborator/new.html.twig', [
+            'collaborator' => $collaborator,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{idUser}', name: 'app_collaborator_show', methods: ['GET'])]
+    public function show(Collaborator $collaborator): Response
+    {
+        return $this->render('collaborator/show.html.twig', [
+            'collaborator' => $collaborator,
+        ]);
+    }
+
+    #[Route('/{idUser}/edit', name: 'app_collaborator_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Collaborator $collaborator, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CollaboratorType::class, $collaborator);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_collaborator_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('collaborator/edit.html.twig', [
+            'collaborator' => $collaborator,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{idUser}', name: 'app_collaborator_delete', methods: ['POST'])]
+    public function delete(Request $request, Collaborator $collaborator, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$collaborator->getIdUser(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($collaborator);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_collaborator_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/CollaboratorType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Collaborator;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CollaboratorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('password', PasswordType::class)
+            ->add('post')
+            ->add('team');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Collaborator::class,
+        ]);
+    }
+}
